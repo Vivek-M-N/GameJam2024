1dacf8468c28b83ce16d688b4c02ff704d503473
Vivek M N <mnvivek2k14@gmail.com>
2024-07-12 14:37:49 +0200

    Update env.py

--- env.py
+++ env.py
@@ -19,16 +19,26 @@ class GameView(arcade.View):
         self.start_y = 0
         self.current_x = 0
         self.current_y = 0
+        
+        wall_width=10
+        wall_height=10
+        
+        self.walls = [
+            (500, (SCREEN_HEIGHT-wall_width)/2, wall_width+10, wall_height),
+            (500, (SCREEN_HEIGHT)/2, wall_width+10, wall_height),
+            (500, (SCREEN_HEIGHT+wall_width)/2, wall_width+10, wall_height)]
 
     def on_draw(self):
         arcade.start_render()
         # arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, (0, 0, 0))
         self.ball.draw()
-        
         if self.mouse_pressed:
             arcade.draw_line(self.start_x, self.start_y, self.current_x, self.current_y, arcade.color.WHITE)
             self.draw_trajectory(self.start_x, self.start_y, self.current_x, self.current_y)
-
+            
+        for wall in self.walls:
+            arcade.draw_rectangle_filled(wall[0], wall[1], wall[2], wall[3], arcade.color.ALLOY_ORANGE)
+            
     def on_mouse_press(self, x, y, button, modifiers):
         self.mouse_pressed = True
         self.start_x = x
