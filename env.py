0086112e8533ecbec8f3f65a5dd6d498739da0a3
AbhishekKolekar08 <152891859+AbhishekKolekar08@users.noreply.github.com>
2024-07-12 12:51:41 +0200

    Update env.py

--- env.py
+++ env.py
@@ -16,6 +16,24 @@ class GameView(arcade.View):
         arcade.start_render()
         arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
 
+class Circle:
+    count = 1
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.selected = False
+        self.color = (255, 0, 0)
+        self.RADIUS = 300
+        self.name = str(Circle.count)
+        Circle.count += 1
+
+    def draw(self):
+        if self.selected:
+            arcade.draw_circle_outline(self.x, self.y, self.RADIUS, arcade.color.WHITE)
+        arcade.draw_circle_filled(self.x, self.y, self.RADIUS, self.color)
+        arcade.draw_text(self.name, self.x - 7, self.y - 7, arcade.color.WHITE, font_size=15)
+
 def main():
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     #window.menu_view = StartScreen()
