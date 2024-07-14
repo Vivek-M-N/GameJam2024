d1f975b7705f792bfd51ccc3e9fac1da8f11873e
Vivek M N <mnvivek2k14@gmail.com>
2024-07-14 09:59:48 +0200

    Fixed tk issue, added start page

--- Physics at Home/main.py
+++ Physics at Home/main.py
@@ -2,6 +2,7 @@ import arcade
 import os
 import pygame
 from tkinter import messagebox
+import tkinter as tk
 from config_app import ConfigApp
 from levels.level1 import Level1
 
@@ -10,6 +11,34 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 SCREEN_TITLE = "Ball toss (but with physics)"
 
+class Start(arcade.View):
+    
+    def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT):
+        super().__init__()
+        self.background = arcade.load_texture("assets/title page.png")
+        self.bg_scale = self.background.width / SCREEN_WIDTH
+        self.main_menu = MainMenu()
+
+    def on_show(self):
+        arcade.set_background_color(arcade.color.WHITE)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.P:
+            self.window.show_view(self.main_menu)
+            
+    def on_mouse_press(self, x, y, button, modifiers):
+         self.mouse_pressed = True
+
+    def on_mouse_release(self, x, y, button, modifiers):
+         self.mouse_pressed = False
+         self.mouse_released = True
+         self.window.show_view(self.main_menu)
+         
+
 class MainMenu(arcade.View):
 
     def on_show(self):
@@ -17,11 +46,13 @@ class MainMenu(arcade.View):
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("Main Menu", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 50,
+        arcade.draw_text("Instructions", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 50,
                          arcade.color.BLACK, font_size=30, anchor_x="center")
         arcade.draw_text("Press P to Play", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
                          arcade.color.BLACK, font_size=20, anchor_x="center")
         arcade.draw_text("Press E to Experiment", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 50,
+                         arcade.color.BLACK, font_size=20, anchor_x="center")
+        arcade.draw_text("Press R to Reset", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 100,
                          arcade.color.BLACK, font_size=20, anchor_x="center")
 
     def on_key_press(self, key, modifiers):
@@ -40,7 +71,8 @@ def main():
     pygame.mixer.music.play(-1)  # Play music indefinitely (-1)
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     main_menu = MainMenu()
-    window.show_view(main_menu)
+    start = Start(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window.show_view(start)
     arcade.run()
 
 if __name__ == "__main__":
